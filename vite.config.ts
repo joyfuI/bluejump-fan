ff015692ba611b879c59ba6562fe9db8e7bfe9a9
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-03-25 20:18:01 +0900

    패키지 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,24 @@
 import { fileURLToPath, URL } from 'node:url';
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
-import viteReact from '@vitejs/plugin-react';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { nitro } from 'nitro/vite';
 import { defineConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config = defineConfig({
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+    tsconfigPaths: true,
+  },
   plugins: [
     devtools(),
     nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
-    viteReact({ babel: { plugins: ['babel-plugin-react-compiler'] } }),
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 });
 
