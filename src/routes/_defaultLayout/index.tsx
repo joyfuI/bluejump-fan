ec4254cc0e7bb33b62ab9e35314ee303d9a28f89
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-05-31 16:29:49 +0900

    라이브 모아보기와 다시보기 모아보기는 공홈으로 대체

--- src/routes/_defaultLayout/index.tsx
+++ src/routes/_defaultLayout/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_defaultLayout/')({
   beforeLoad: () => {
-    throw Route.redirect({ to: '/live', replace: true });
+    throw Route.redirect({ to: '/calendar', replace: true });
   },
   component: () => null,
 });
