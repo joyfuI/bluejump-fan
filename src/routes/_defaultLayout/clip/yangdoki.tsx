0aa51237561b21b9d422c26b1f781ca51261a1a2
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-08-21 23:20:17 +0900

    클립 검색기 키워드 업데이트

--- src/routes/_defaultLayout/clip/yangdoki.tsx
+++ src/routes/_defaultLayout/clip/yangdoki.tsx
@@ -11,7 +11,7 @@ const RouteComponent = () => {
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useClipQuery(
     '도끼',
-    ['도끼던지', '풍도끼'],
+    ['도끼던지', '풍도끼', '돌도끼'],
     ['yangdoki'],
   );
 
