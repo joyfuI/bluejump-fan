8a69d4dc74258f5a4f1e5cd08920f8aebccfa468
joyfuI <1342862+joyfuI@users.noreply.github.com>
2026-08-28 02:10:01 +0900

    클립 검색기 키워드 업데이트

--- src/routes/_defaultLayout/clip/wjdfogur98.tsx
+++ src/routes/_defaultLayout/clip/wjdfogur98.tsx
@@ -11,7 +11,7 @@ const RouteComponent = () => {
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } = useClipQuery(
     '월향',
-    ['뚜부네월향', 'JINU월향'],
+    ['뚜부네월향', 'JINU월향', '월향오빠', '월향이오빠'],
     ['wjdfogur98'],
   );
 
